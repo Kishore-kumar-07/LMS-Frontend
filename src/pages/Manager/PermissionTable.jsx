12b6e512b6e3797be44680f8739a53a23fef877f
Lingeshwaran <lingeshwaran.kv2022cse@sece.ac.in>
2024-11-06 20:54:57 +0530

    made sma changes in required code

--- src/pages/Manager/PermissionTable.jsx
+++ src/pages/Manager/PermissionTable.jsx
@@ -33,17 +33,11 @@ const PermissionTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedReason, setSelectedReason] = useState(null);
   const [isPopupOpen, setPopupOpen] = useState(false);
-  const [editPopupOpen, setEditPopupOpen] = useState(false);
-  const [selectedRow, setSelectedRow] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [statusFilter, setStatusFilter] = useState("All");
+  // const [editPopupOpen, setEditPopupOpen] = useState(false);
+   const [filteredData, setFilteredData] = useState([]);
   const [status, setStatus] = useState(CURRENT_STATUS.IDEAL);
   const [editRowId, setEditRowId] = useState(null);
 
-  const rowsPerPage = 6; // Adjust as needed
-  const totalPages = Math.ceil(data.length / rowsPerPage);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -208,15 +202,12 @@ const PermissionTable = () => {
     }
   };
 
-  const startIndex = (currentPage - 1) * rowsPerPage;
-  const endIndex = startIndex + rowsPerPage;
-  const dataToDisplay = filteredData.slice(startIndex, endIndex);
 
   return (
     <div className="w-[100%] p-3 border-slate-950 rounded-lg">
       <ToastContainer />
-
       <div className="w-[100%] overflow-x-auto">
+      <div className="max-h-[400px] overflow-y-auto">
         <table className="w-full divide-y divide-gray-200 bg-white">
           <thead className="bg-gray-50">
             <tr>
@@ -231,10 +222,10 @@ const PermissionTable = () => {
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {dataToDisplay.map((row, rowIndex) => (
+            {filteredData.map((row, rowIndex) => (
               <tr key={rowIndex + 1} className="h-fit">
                 <td className=" text-md font-medium  text-gray-900 px-2 py-2">
-                  {startIndex + rowIndex + 1}
+                  {rowIndex+1}
                 </td>
                 <td className="px-2 py-2 whitespace-nowrap text-md font-medium text-gray-900 justify-center items-center">
                   {row.empName}
@@ -317,22 +308,19 @@ const PermissionTable = () => {
             ))}
           </tbody>
         </table>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
+        
+      </div>
       </div>
       <Popup
         isOpen={isPopupOpen}
         onClose={() => setPopupOpen(false)}
         content={selectedReason}
       />
-      <EditPopup
+      {/* <EditPopup
         isOpen={editPopupOpen}
         onClose={() => setEditPopupOpen(false)}
         row={selectedRow}
-      />
+      /> */}
     </div>
   );
 };
@@ -365,62 +353,62 @@ const Popup = ({ isOpen, onClose, content }) => {
   );
 };
 
-const EditPopup = ({ isOpen, onClose, row, onStatusChange }) => {
-  if (!isOpen || !row) return null;
+// const EditPopup = ({ isOpen, onClose, row, onStatusChange }) => {
+//   if (!isOpen || !row) return null;
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center z-50">
+//       <div className="absolute inset-0 bg-black opacity-50"></div>
 
-      <div className="bg-white text-black p-6 rounded-lg shadow-lg z-10 max-w-lg w-full">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-bold text-gray-900">Edit Permission</h2>
-          <button onClick={onClose} className="text-black hover:text-gray-500">
-            <MdClose size={24} />
-          </button>
-        </div>
-        <div className="text-black">
-          <p>Employee: {row.empName}</p>
-          <p>Type: Permission</p>
-          <p>From: {row.from}</p>
-          <p>To: {row.to}</p>
-          <p>
-            Hours:{" "}
-            {row.hrs < 1
-              ? `${(row.hrs * 60).toFixed(2)} mins`
-              : `${row.hrs.toFixed(2)} hrs`}
-          </p>
-          <p>Reason: {row.reason}</p>
-        </div>
-        <div className="mt-4 flex justify-end gap-2">
-          <button
-            onClick={() => {
-              onStatusChange(row._id, "Approved");
-              onClose();
-            }}
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-          >
-            Approve
-          </button>
-          <button
-            onClick={() => {
-              onStatusChange(row._id, "Denied");
-              onClose();
-            }}
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-          >
-            Reject
-          </button>
-          <button
-            onClick={onClose}
-            className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+//       <div className="bg-white text-black p-6 rounded-lg shadow-lg z-10 max-w-lg w-full">
+//         <div className="flex justify-between items-center mb-4">
+//           <h2 className="text-xl font-bold text-gray-900">Edit Permission</h2>
+//           <button onClick={onClose} className="text-black hover:text-gray-500">
+//             <MdClose size={24} />
+//           </button>
+//         </div>
+//         <div className="text-black">
+//           <p>Employee: {row.empName}</p>
+//           <p>Type: Permission</p>
+//           <p>From: {row.from}</p>
+//           <p>To: {row.to}</p>
+//           <p>
+//             Hours:{" "}
+//             {row.hrs < 1
+//               ? `${(row.hrs * 60).toFixed(2)} mins`
+//               : `${row.hrs.toFixed(2)} hrs`}
+//           </p>
+//           <p>Reason: {row.reason}</p>
+//         </div>
+//         <div className="mt-4 flex justify-end gap-2">
+//           <button
+//             onClick={() => {
+//               onStatusChange(row._id, "Approved");
+//               onClose();
+//             }}
+//             className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+//           >
+//             Approve
+//           </button>
+//           <button
+//             onClick={() => {
+//               onStatusChange(row._id, "Denied");
+//               onClose();
+//             }}
+//             className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+//           >
+//             Reject
+//           </button>
+//           <button
+//             onClick={onClose}
+//             className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+//           >
+//             Cancel
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default PermissionTable;
