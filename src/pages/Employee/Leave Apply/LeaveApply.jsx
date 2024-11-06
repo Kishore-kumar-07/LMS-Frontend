ac63a37a2ed621c6e83e23f232e890e72ab0317c
Kishore-kumar-07 <bkdon54@gmail.com>
2024-11-06 13:55:55 +0530

    deploy check

--- src/pages/Employee/Leave Apply/LeaveApply.jsx
+++ src/pages/Employee/Leave Apply/LeaveApply.jsx
@@ -5,7 +5,7 @@ import Leaveform from "./Leaveform";
 function LeaveApply({ isPaternity, isAdoption }) {
   return (
     <div className="w-full h-full flex items-center justify-center " >
-      <div className="w-full  flex flex-col lg:flex-row justify-center items-center lg:justify-between gap-8 p-16">
+      <div className="w-full  flex flex-col lg:flex-row justify-center items-center lg:justify-between gap-5 px-10 ">
         <Leaveform isPaternity={isPaternity} isAdoption={isAdoption} />
         <LeaveDetailTable />
       </div>
